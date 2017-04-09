08536c9fd68d433c333c761eb154b6b05c42c6ca
zoeywang <zwang297@gmail.com>
2017-04-09 17:15:47 -0400

    New project (#1)

---
+++ app/src/main/java/com/njitdev/sa_android/utils/SAConfig.java
@@ -0,0 +1,9 @@
+package com.njitdev.sa_android.utils;
+
+/**
+ * Created by WZ on 4/9/17.
+ */
+
+public class SAConfig {
+    public static String baseURL = "https://sa-api-dev.njitdev.com";
+}
---
+++ app/src/main/java/com/njitdev/sa_android/utils/SAGlobal.java
@@ -0,0 +1,16 @@
+package com.njitdev.sa_android.utils;
+
+import com.android.volley.RequestQueue;
+
+/**
+ * Created by WZ on 4/9/17.
+ */
+
+public class SAGlobal {
+    private static final SAGlobal ourInstance = new SAGlobal();
+    public RequestQueue sharedRequestQueue;
+    static SAGlobal getInstance() {
+        return ourInstance;
+    }
+    private SAGlobal() {}
+}
