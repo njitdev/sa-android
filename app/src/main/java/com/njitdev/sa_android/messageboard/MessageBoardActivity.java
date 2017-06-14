2d1973d051f20014cc89c23fca4a296e101ebbc9
Yunzhu Li <contact@yunzhu.li>
2017-06-13 23:14:46 -0400

    Message board changes and cleanup (#15)
    * Message board changes and cleanup
    
    * font update
    
    * Limit text input box height

--- app/src/main/java/com/njitdev/sa_android/messageboard/MessageBoardActivity.java
+++ app/src/main/java/com/njitdev/sa_android/messageboard/MessageBoardActivity.java
@@ -1,13 +1,19 @@
 package com.njitdev.sa_android.messageboard;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -16,14 +22,16 @@ import android.widget.Toast;
 
 import com.njitdev.sa_android.R;
 import com.njitdev.sa_android.utils.ModelListener;
+import com.njitdev.sa_android.utils.SAGlobal;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MessageBoardActivity extends AppCompatActivity {
 
-    MessageAdapter messageAdapter;
+    MessageBoardAdapter mAdapter;
     ArrayList<Post> mPosts = new ArrayList<>();
-    int pageNum = 0; // Current page number
+    int mPageNum = 0; // Current page number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,7 @@ public class MessageBoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message_board);
 
         initListView();
-        fetchMessage(pageNum);
+        fetchMessage(mPageNum);
     }
 
     @Override
@@ -49,7 +57,7 @@ public class MessageBoardActivity extends AppCompatActivity {
                 return true;
 
             case R.id.MessageBoardnewMessage:
-                newPost();
+                newPostDialog();
                 return true;
         }
         return onOptionsItemSelected(item);
@@ -59,8 +67,8 @@ public class MessageBoardActivity extends AppCompatActivity {
         final ListView listView = (ListView) findViewById(R.id.listViewMessageboardText);
 
         // Create adapter
-        messageAdapter = new MessageAdapter(listView.getContext(), R.layout.message_board_rowcontext, mPosts);
-        listView.setAdapter(messageAdapter);
+        mAdapter = new MessageBoardAdapter(listView.getContext(), R.layout.list_item_message_board, mPosts);
+        listView.setAdapter(mAdapter);
 
         // Set listener for scroll down;
         listView.setOnScrollListener(new AbsListView.OnScrollListener() {
@@ -68,9 +76,9 @@ public class MessageBoardActivity extends AppCompatActivity {
             public void onScrollStateChanged(AbsListView view, int scrollState) {
                 if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE
                         && (listView.getLastVisiblePosition() - listView.getHeaderViewsCount() -
-                        listView.getFooterViewsCount()) >= (messageAdapter.getCount() - 1)) {
-                    pageNum++;
-                    fetchMessage(pageNum);
+                        listView.getFooterViewsCount()) >= (mAdapter.getCount() - 1)) {
+                    mPageNum++;
+                    fetchMessage(mPageNum);
                 }
             }
 
@@ -80,7 +88,7 @@ public class MessageBoardActivity extends AppCompatActivity {
         });
     }
 
-    private void setBusy(boolean busy) {
+    private void setUIBusy(boolean busy) {
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressLoading);
         if (busy) {
             progressBar.setVisibility(View.VISIBLE);
@@ -89,45 +97,46 @@ public class MessageBoardActivity extends AppCompatActivity {
         }
     }
 
-    //fetch json data and show
+    // Fetch and display list
     private void fetchMessage(int page) {
-        setBusy(true);
-        MessageBoardModels.fetchList(page, new ModelListener() {
+        setUIBusy(true);
+        MessageBoardModels.fetchList(page, new ModelListener<List<Post>>() {
             @Override
-            public void onData(Object result, String message) {
-                setBusy(false);
+            public void onData(List<Post> result, String message) {
+                setUIBusy(false);
 
                 if (result == null) {
                     Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
                 } else {
-                    ArrayList<Post> posts = (ArrayList<Post>) result;
-                    mPosts.addAll(posts);
+                    mPosts.addAll(result);
 
                     // Update ListView
-                    messageAdapter.notifyDataSetChanged();
+                    mAdapter.notifyDataSetChanged();
                 }
             }
         });
     }
 
     // New post dialog
-    public void newPost() {
+    public void newPostDialog() {
         // Create dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(MessageBoardActivity.this);
-        View view = getLayoutInflater().inflate(R.layout.activity_new_message, null);
-        builder.setView(view);
+        final View dialogView = getLayoutInflater().inflate(R.layout.dialog_new_message, (ViewGroup) this.findViewById(R.id.content));
+        builder.setView(dialogView);
         final AlertDialog dialog = builder.create();
         dialog.show();
 
-        // Check and get input value
-        final TextView txtUserName = (TextView) view.findViewById(R.id.txtUserName);
-        final TextView txtUserContact = (TextView) view.findViewById(R.id.txtUserContact);
-        final TextView txtPostText = (TextView) view.findViewById(R.id.txtPostText);
-        Button btnSubmitMessage = (Button) view.findViewById(R.id.btnSubmitMessage);
+        // Dialog buttons
+        Button btnSubmitMessage = (Button) dialogView.findViewById(R.id.btnSubmitMessage);
+        Button btnCancel = (Button) dialogView.findViewById(R.id.btnCancel);
 
         btnSubmitMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Get user input
+                TextView txtUserName = (TextView) dialogView.findViewById(R.id.lblUserName);
+                TextView txtUserContact = (TextView) dialogView.findViewById(R.id.txtUserContact);
+                TextView txtPostText = (TextView) dialogView.findViewById(R.id.txtPostText);
                 String user_name = txtUserName.getText().toString();
                 String user_contact = txtUserContact.getText().toString();
                 String text = txtPostText.getText().toString();
@@ -136,26 +145,69 @@ public class MessageBoardActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "用户名或留言不能为空", Toast.LENGTH_LONG).show();
                 } else {
                     Post post = new Post();
+                    post.installation_id = SAGlobal.installation_id;
                     post.user_name = user_name;
                     post.text = text;
                     post.user_contact = user_contact;
 
-                    setBusy(true);
-                    MessageBoardModels.submitPost("IDbyIris", post, new ModelListener() {
+                    setUIBusy(true);
+
+                    // Submit
+                    MessageBoardModels.submitPost(post, new ModelListener<Boolean>() {
                         @Override
-                        public void onData(Object result, String message) {
-                            setBusy(false);
+                        public void onData(Boolean result, String message) {
+                            setUIBusy(false);
 
                             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
                             if (result == Boolean.TRUE) {
+                                // Reload list
                                 mPosts.clear();
                                 fetchMessage(0);
                             }
                         }
                     });
-                    dialog.cancel();
+                    dialog.dismiss();
                 }
             }
         });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+    }
+}
+
+class MessageBoardAdapter extends ArrayAdapter<Post> {
+
+    private Context mContext;
+    private List<Post> mPosts;
+
+    MessageBoardAdapter(Context context, int resource, List<Post> posts) {
+        super(context, resource, posts);
+        mContext = context;
+        mPosts = posts;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item_message_board, parent, false);
+        }
+
+        TextView lblUserName = (TextView) convertView.findViewById(R.id.lblUserName);
+        TextView lblUserTitle = (TextView) convertView.findViewById(R.id.lblUserTitle);
+        TextView lblMessage = (TextView) convertView.findViewById(R.id.lblMessage);
+
+        Post post = mPosts.get(position);
+
+        lblUserName.setText(post.user_name);
+        lblMessage.setText(post.text);
+        lblUserTitle.setText(post.user_title == null ? "" : post.user_title);
+
+        return convertView;
     }
 }
