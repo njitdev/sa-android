2f56914b2a05c533a637ef3c6c66195ea7cacaad
Meiyan Xie <meiyan@blupig.net>
2017-07-12 12:48:45 +0800

    Basic library functions (#17)
    * Update Gradle version
    
    * library activity and models
    
    * add customized adapter
    
    * add details activity
    
    * library basic function work
    
    * library almost done
    
    * slightly modify the layout
    
    * add code of hiding keyboard
    
    * add progress bar and test null value
    
    * set null to be --
    
    * set null to be space
    
    * remove a extra textview
    
    * Check empty keyword
    
    * fix issues

---
+++ app/src/main/java/com/njitdev/sa_android/library/LibraryActivity.java
@@ -0,0 +1,172 @@
+package com.njitdev.sa_android.library;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.njitdev.sa_android.R;
+import com.njitdev.sa_android.utils.ModelListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    private List<Book> mBooks = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+
+        // Set not busy
+        setUIBusy(false);
+
+        // Create adapter
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter libraryAdapter = new LibraryAdapter(this, R.layout.list_item_library_search, mBooks);
+        listView.setAdapter(libraryAdapter);
+
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        final EditText txtKeyword = (EditText) findViewById(R.id.txtKeyword);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Check the input
+                if (txtKeyword.getText().toString().isEmpty()) {
+                    Toast.makeText(LibraryActivity.this, "请输入关键字", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Set busy
+                    setUIBusy(true);
+
+                    LibraryModels.search(txtKeyword.getText().toString(), new ModelListener() {
+                        @Override
+                        public void onData(Object result, String message) {
+
+                            // Set not busy
+                            setUIBusy(false);
+
+                            if (result == null) {
+                                Toast.makeText(LibraryActivity.this, message, Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            // Update data of adapter
+                            mBooks.clear();
+                            mBooks.addAll((ArrayList<Book>) result);
+                            libraryAdapter.notifyDataSetChanged();
+
+                            // Hide keyboard
+                            View view = LibraryActivity.this.getCurrentFocus();
+                            if (view != null) {
+                                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(LibraryActivity.this, BookDetailsActivity.class);
+                Book book = mBooks.get(position);
+
+                // Pass book info to details activity
+                intent.putExtra("book_id", book.id);
+                intent.putExtra("book_title", book.title);
+                intent.putExtra("book_author", book.author);
+                intent.putExtra("book_publisher", book.publisher);
+                intent.putExtra("book_year", book.year);
+                intent.putExtra("book_acquisition_number", book.acquisition_number);
+                intent.putExtra("book_inventory", book.inventory);
+                intent.putExtra("book_available", book.available);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setUIBusy(Boolean busy) {
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        EditText txtKeyword = (EditText) findViewById(R.id.txtKeyword);
+
+        // Enable / disable
+        btnSearch.setEnabled(!busy);
+        txtKeyword.setEnabled(!busy);
+
+        // Spinner
+        ProgressBar pbSearch = (ProgressBar) findViewById(R.id.pbSearch);
+        if (busy) {
+            pbSearch.setVisibility(View.VISIBLE);
+        } else {
+            pbSearch.setVisibility(View.INVISIBLE);
+        }
+    }
+}
+
+class LibraryAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private int mResource;
+    private List<Book> mBooks;
+
+    public LibraryAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Book> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+        mBooks = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(mResource, parent, false);
+        }
+
+        // Controls
+        TextView lblTitle = (TextView) convertView.findViewById(R.id.lblTitle);
+        TextView lblAuthor = (TextView) convertView.findViewById(R.id.lblAuthor);
+        TextView lblInventory = (TextView) convertView.findViewById(R.id.lblInventory);
+        TextView lblAvailable = (TextView) convertView.findViewById(R.id.lblAvailable);
+        TextView lblYear = (TextView) convertView.findViewById(R.id.lblYear);
+        TextView lblPublisher = (TextView) convertView.findViewById(R.id.lblPublisher);
+        TextView lblAcqNum = (TextView) convertView.findViewById(R.id.lblAcqNum);
+
+        // Data
+        Book b = mBooks.get(position);
+        lblTitle.setText(b.title);
+        lblAuthor.setText(b.author);
+        String storeNum = "馆藏:" + b.inventory;
+        String availableNum = "可借:" + b.available;
+        lblInventory.setText(storeNum);
+        lblAvailable.setText(availableNum);
+        lblYear.setText(b.year);
+        lblPublisher.setText(b.publisher);
+        lblAcqNum.setText(b.acquisition_number);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/njitdev/sa_android/library/LibraryModels.java
@@ -0,0 +1,116 @@
+package com.njitdev.sa_android.library;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.njitdev.sa_android.utils.ModelListener;
+import com.njitdev.sa_android.utils.SAConfig;
+import com.njitdev.sa_android.utils.SAGlobal;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by meiyan on 6/10/17.
+ */
+
+class LibraryModels {
+    private static String baseURL = SAConfig.baseURL + "/library/" + SAConfig.schoolIdentifier;
+
+    public static void search(String keyword, final ModelListener listener) {
+        JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, baseURL + "/search?keyword=" + keyword, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                ArrayList<Book> result = new ArrayList<>();
+                try {
+                    JSONArray books = response.getJSONArray("result");
+                    for (int i = 0; i < books.length(); i++) {
+                        JSONObject jsonBook = books.getJSONObject(i);
+                        Book book = new Book();
+                        book.id = jsonBook.getString("id");
+                        book.title = jsonBook.getString("title");
+
+                        if (jsonBook.has("author") && !jsonBook.isNull("author")) book.author = jsonBook.getString("author");
+                        if (jsonBook.has("publisher") && !jsonBook.isNull("publisher")) book.publisher = jsonBook.getString("publisher");
+                        if (jsonBook.has("year") && !jsonBook.isNull("year")) book.year = jsonBook.getString("year");
+                        if (jsonBook.has("acquisition_number") && !jsonBook.isNull("acquisition_number")) book.acquisition_number = jsonBook.getString("acquisition_number");
+                        if (jsonBook.has("inventory") && !jsonBook.isNull("inventory")) book.inventory = jsonBook.getString("inventory");
+                        if (jsonBook.has("available") && !jsonBook.isNull("available")) book.available = jsonBook.getString("available");
+                        result.add(book);
+                    }
+                    listener.onData(result, "OK");
+                } catch (JSONException e) {
+                    listener.onData(null, "解析数据失败");
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.onData(null, "连接学校服务器超时");
+            }
+        });
+        SAGlobal.sharedRequestQueue.add(r);
+    }
+
+    public static void details(String bookId, final ModelListener listener) {
+        JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, baseURL + "/details?book_id=" +bookId, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // Parse response here
+                ArrayList<BookInventory> result = new ArrayList<>();
+                try {
+                    JSONArray bookInventories = response.getJSONObject("result").getJSONArray("inventory");
+                    for (int i = 0; i < bookInventories.length(); i++) {
+                        JSONObject jsonDetails = bookInventories.getJSONObject(i);
+                        BookInventory bookInventory = new BookInventory();
+
+                        if (jsonDetails.has("location") && !jsonDetails.isNull("location")) bookInventory.location = jsonDetails.getString("location");
+                        if (jsonDetails.has("login_number") && !jsonDetails.isNull("login_number")) bookInventory.login_number = jsonDetails.getString("login_number");
+                        if (jsonDetails.has("year") && !jsonDetails.isNull("year")) bookInventory.year = jsonDetails.getString("year");
+                        if (jsonDetails.has("acquisition_number") && !jsonDetails.isNull("acquisition_number")) bookInventory.acquisition_number = jsonDetails.getString("acquisition_number");
+                        if (jsonDetails.has("type") && !jsonDetails.isNull("type")) bookInventory.type = jsonDetails.getString("type");
+                        if (jsonDetails.has("inventory") && !jsonDetails.isNull("inventory")) bookInventory.inventory = jsonDetails.getString("inventory");
+                        if (jsonDetails.has("availability") && !jsonDetails.isNull("availability")) bookInventory.availability = jsonDetails.getString("availability");
+                        result.add(bookInventory);
+                    }
+                    listener.onData(result, "OK");
+                } catch (JSONException e) {
+                    listener.onData(null, "解析数据失败");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.onData(null, "连接学校服务器超时");
+            }
+        });
+        SAGlobal.sharedRequestQueue.add(r);
+    }
+}
+
+class Book {
+    String id;
+    String title;
+    String author;
+    String publisher;
+    String year;
+    String acquisition_number;
+    String inventory;
+    String available;
+}
+
+class BookInventory {
+    String location;
+    String login_number;
+    String year;
+    String acquisition_number;
+    String type;
+    String inventory;
+    String availability;
+}
