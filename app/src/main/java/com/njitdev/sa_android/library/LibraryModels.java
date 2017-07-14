cfbc3c7499f506ac977934c3203f33cd389e6ec7
Yunzhu Li <contact@yunzhu.li>
2017-07-14 01:34:50 -0400

    Add GPL headers

--- app/src/main/java/com/njitdev/sa_android/library/LibraryModels.java
+++ app/src/main/java/com/njitdev/sa_android/library/LibraryModels.java
@@ -1,3 +1,21 @@
+/*
+    sa-android
+    Copyright (C) 2017 sa-android authors
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.njitdev.sa_android.library;
 
 import com.android.volley.Request;
@@ -13,10 +31,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-
-/**
- * Created by meiyan on 6/10/17.
- */
 
 class LibraryModels {
     private static String baseURL = SAConfig.baseURL + "/library/" + SAConfig.schoolIdentifier;
--- app/src/main/java/com/njitdev/sa_android/utils/ModelListener.java
+++ app/src/main/java/com/njitdev/sa_android/utils/ModelListener.java
@@ -1,9 +1,22 @@
-package com.njitdev.sa_android.utils;
+/*
+    sa-android
+    Copyright (C) 2017 sa-android authors
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-/**
- * Created by WZ on 4/9/17.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package com.njitdev.sa_android.utils;
 
 public interface ModelListener<T> {
     void onData(T result, String message);
