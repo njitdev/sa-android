1325b99fc80d7806ef57a2b364137a2af4701136
Yunzhu Li <contact@yunzhu.li>
2017-07-28 00:17:42 -0400

    Add Google Analytics

--- app/src/main/java/com/njitdev/sa_android/library/LibraryBookDetailsActivity.java
+++ app/src/main/java/com/njitdev/sa_android/library/LibraryBookDetailsActivity.java
@@ -19,11 +19,11 @@
 package com.njitdev.sa_android.library;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
