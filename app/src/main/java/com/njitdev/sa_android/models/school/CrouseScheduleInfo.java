b9977d8d31fd1bda35999642468cc43233eafc6e
zoey97wang <zwang297@gmail.com>
2017-08-05 18:48:58 -0400

    fix_conflict

---
+++ app/src/main/java/com/njitdev/sa_android/models/school/CrouseScheduleInfo.java
@@ -0,0 +1,15 @@
+package com.njitdev.sa_android.models.school;
+
+/**
+ * Created by WZ on 8/5/17.
+ */
+
+public class Syllabus {
+    int week;
+    int day_of_week;
+    String classes_in_day;
+    String title;
+    String instructor;
+    String location;
+    String type;
+}
